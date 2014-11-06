1190f9f6f3f5af09adf304dd0137ccce753732ae
jennybc <jenny@stat.ubc.ca>
2014-11-06 12:35:16 -0800

    readLines not read.delim

--- block023_pipelines/activity/histogram.r
+++ block023_pipelines/activity/histogram.r
@@ -1,5 +1,5 @@
-words <- read.delim("words.tsv", stringsAsFactors = FALSE)[[1]]
+words <- readLines("words.txt")
 Length <- nchar(words)
-counts <- table(Length)
-write.table(counts, "histogram.tsv",
-	sep = "\t", row.names = FALSE, quote = FALSE)
+hist_dat <- table(Length)
+write.table(hist_dat, "histogram.tsv",
+            sep = "\t", row.names = FALSE, quote = FALSE)
