d7a6cd699924509091e6886a6e8fbea32b61cf91
Jennifer (Jenny) Bryan <jenny@stat.ubc.ca>
2014-11-09 22:20:33 -0800

    whitespace

--- automation10_holding-area/01_automation-example_just-r/Makefile.R
+++ automation10_holding-area/01_automation-example_just-r/Makefile.R
@@ -2,8 +2,8 @@
 
 ## clean out any previous work
 outputs <- c("lotr_raw.tsv",                     # 00_download-data.R
-						 "lotr_clean.tsv",                   # 01_filter-reorder-plot.R
-						 "total-words-by-film-race.tsv",     # 02_aggregate-plot.R
+	     "lotr_clean.tsv",                   # 01_filter-reorder-plot.R
+	     "total-words-by-film-race.tsv",     # 02_aggregate-plot.R
              list.files(pattern = "*.png$"))
 file.remove(outputs)
 
