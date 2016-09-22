c999f9115845d8763849e813eac1c2b0205ab337
jennybc <jenny@stat.ubc.ca>
2016-09-22 15:16:26 -0700

    add indicative live code from this week

--- cm005_hello-gapminder-tidyverse.R
+++ cm005_hello-gapminder-tidyverse.R
@@ -1,3 +1,6 @@
+## indicative of some of the live coding done in
+## cm005 and cm006
+## on 2016-09-20 and 2016-09-22
 library(tidyverse)
 library(gapminder)
 
@@ -9,8 +12,30 @@ head(gapminder)
 tail(gapminder)
 
 filter(gapminder, country == "Canada")
+
+## I got tugged in various directions by some good questions
+filter(gapminder, year > 2000)
+filter(gapminder, continent == "Europe", year == 2007)
+filter(gapminder, country == "Bulgaria" | year == 2007)
+filter(gapminder, country == "Bulgaria" | country == "Albania")
+filter(gapminder, country %in% c("Bulgaria", "Albania"))
+filter(gapminder, grepl("^F", country))
+(canada <- filter(gapminder, country == "Canada"))
+
 select(gapminder, year, lifeExp)
+
 select(filter(gapminder, country == "Canada"), year, lifeExp)
+
+select(
+  filter(
+    gapminder,
+    country == "Canada"
+  ),
+  year,
+  lifeExp
+)
+
+## looked at some %>% slides here
 
 gapminder %>%
   filter(country == "Canada")
@@ -22,18 +47,18 @@ gapminder %>%
   filter(country == "Canada") %>%
   select(year, lifeExp)
 
-## show %>% slides here
+gapminder %>%
+  select(starts_with("co"))
 
+## info about the data frame as a whole
 names(gapminder)
 colnames(gapminder)
-
 ncol(gapminder)
 length(gapminder)
 dim(gapminder)
 nrow(gapminder)
 
 ## shift from describing basic object to variables inside
-
 summary(gapminder)
 
 gapminder$country
@@ -47,6 +72,22 @@ gapminder[["country"]]
 
 ## remember I have the "flavors of objects" slides
 ## and the ones about different modes of working with R
+
+summary(gapminder)
+## main way to get 1 variable: use $
+gapminder$lifeExp
+## another way to get 1 variable: use [[ ]]
+gapminder[["gdpPercap"]]
+## a way to get several variables: use [ ]
+gapminder[c("year", "country")]
+
+my_favorite_variable <- "country"
+gapminder[[my_favorite_variable]]
+gapminder$my_favorite_variable
+
+str(gapminder$lifeExp)
+str(gapminder["lifeExp"])
+str(gapminder[c("year", "country")])
 
 gapminder
 gapminder$country
@@ -65,9 +106,6 @@ gapminder$lifeExp
 summary(gapminder$lifeExp)
 hist(gapminder$lifeExp)
 
-## intro to dplyr
-filter(gapminder, lifeExp < 29)
-filter(gapminder, country == "Rwanda")
-filter(gapminder, country %in% c("Rwanda", "Afghanistan"))
-## FAQ / puzzle
-filter(gapminder, country == c("Rwanda", "Afghanistan"))
+## we ended with a quick start on ggplot2 stuff
+## slides then some scatterplotting
+## https://github.com/jennybc/ggplot2-tutorial/blob/master/gapminder-ggplot2-scatterplot.r
